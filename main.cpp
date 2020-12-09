fec1aa82df00ce86f9cf96ae0902497129b6b40f
Wiebe Cazemier <wiebe@halfgaar.net>
2020-12-09 22:16:08 +0100

    Fixed error checks

--- main.cpp
+++ main.cpp
@@ -74,19 +74,20 @@ int main()
 {
     int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    int optval = 1;
-    check<std::runtime_error>(setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &optval, sizeof(optval)) < 0);
+    // Not needed for now. Maybe I will make multiple accept threads later, with SO_REUSEPORT.
+    //int optval = 1;
+    //check<std::runtime_error>(setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &optval, sizeof(optval)));
 
     int flags = fcntl(listen_fd, F_GETFL);
-    check<std::runtime_error>(fcntl(listen_fd, F_SETFL, flags | O_NONBLOCK ) < 0);
+    check<std::runtime_error>(fcntl(listen_fd, F_SETFL, flags | O_NONBLOCK ));
 
     struct sockaddr_in in_addr;
     in_addr.sin_family = AF_INET;
     in_addr.sin_addr.s_addr = INADDR_ANY;
     in_addr.sin_port = htons(1883);
 
-    check<std::runtime_error>(bind(listen_fd, (struct sockaddr *)(&in_addr), sizeof(struct sockaddr_in)) < 0);
-    check<std::runtime_error>(listen(listen_fd, 1024) < 0);
+    check<std::runtime_error>(bind(listen_fd, (struct sockaddr *)(&in_addr), sizeof(struct sockaddr_in)));
+    check<std::runtime_error>(listen(listen_fd, 1024));
 
     int epoll_fd_accept = check<std::runtime_error>(epoll_create(999));
 
@@ -97,7 +98,7 @@ int main()
 
     ev.data.fd = listen_fd;
     ev.events = EPOLLIN;
-    check<std::runtime_error>(epoll_ctl(epoll_fd_accept, EPOLL_CTL_ADD, listen_fd, &ev) < 0);
+    check<std::runtime_error>(epoll_ctl(epoll_fd_accept, EPOLL_CTL_ADD, listen_fd, &ev));
 
     std::vector<std::shared_ptr<ThreadData>> threads;
 
