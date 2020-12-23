adbc7b3f710270f13d64953f02e70af4bcc158bd
Wiebe Cazemier <wiebe@halfgaar.net>
2020-12-23 16:59:08 +0100

    Temporary ulimit stuff

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <signal.h>
 #include <memory>
 #include <string.h>
+#include <sys/resource.h>
 
 #include "mainapp.h"
 
@@ -29,6 +30,12 @@ static void signal_handler(int signal)
 
 int register_signal_handers()
 {
+    // Quick'n dirty temp. TODO properly, with config file, checks, etc
+    rlim_t rlim = 1000000;
+    printf("Setting ulimit nofile to %ld.\n", rlim);
+    struct rlimit v = { rlim, rlim };
+    setrlimit(RLIMIT_NOFILE, &v);
+
     struct sigaction sa;
     memset(&sa, 0, sizeof (struct sigaction));
     sa.sa_handler = &signal_handler;
