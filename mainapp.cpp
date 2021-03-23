d76529ab1e77c0f7e7c28258f916478b5c076e12
Wiebe Cazemier <wiebe@halfgaar.net>
2021-03-23 22:15:42 +0100

    Version v0.5.2

--- mainapp.cpp
+++ mainapp.cpp
@@ -31,7 +31,7 @@ License along with FlashMQ. If not, see <https://www.gnu.org/licenses/>.
 
 #define MAX_EVENTS 1024
 
-#define VERSION "0.5.1"
+#define VERSION "0.5.2"
 
 MainApp *MainApp::instance = nullptr;
 
