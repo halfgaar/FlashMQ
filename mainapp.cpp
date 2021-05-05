31084bb49123f70f2247dd04fb0686b9aec57f61
Wiebe Cazemier <wiebe@halfgaar.net>
2021-05-05 21:09:14 +0200

    Version 0.6.1

--- mainapp.cpp
+++ mainapp.cpp
@@ -31,7 +31,7 @@ License along with FlashMQ. If not, see <https://www.gnu.org/licenses/>.
 
 #define MAX_EVENTS 1024
 
-#define VERSION "0.6.0"
+#define VERSION "0.6.1"
 
 MainApp *MainApp::instance = nullptr;
 
