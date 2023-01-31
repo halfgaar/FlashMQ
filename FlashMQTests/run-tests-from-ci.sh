b1df4e6cc93d6c8bf58342aafeabc503027830dc
Wiebe Cazemier <wiebe@halfgaar.net>
2023-01-31 20:56:15 +0100

    Fix failure to detect test failures

---
+++ FlashMQTests/run-tests-from-ci.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+STDERR_LOG=$(mktemp)
+
+if ./FlashMQTests -v1 2> "$STDERR_LOG" ; then
+  echo -e '\033[01;32mSUCCESS!\033[00m'
+else
+  echo -e '\033[01;31mBummer\033[00m'
+  echo -e "\n\nTail of stderr:\n\n"
+  tail -n 200 "$STDERR_LOG"
+  exit 1
+fi
