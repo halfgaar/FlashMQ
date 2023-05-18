159034cb8cba6f1a659dedf24b910845b79013be
Wiebe Cazemier <wiebe@halfgaar.net>
2023-05-18 12:38:09 +1000

    Do fixed size reservation in string splitter
    It's actually detrimental to find the exact count.

--- utils.cpp
+++ utils.cpp
@@ -287,9 +287,8 @@ void splitTopic(const std::string &topic, std::vector<std::string> &output)
 void splitToVector(const std::string &input, std::vector<std::string> &output, const char sep, size_t max, bool keep_empty_parts)
 {
     output.clear();
-    const auto subtopic_count = std::count(input.begin(), input.end(), sep) + 1;
+    output.reserve(16);
 
-    output.reserve(subtopic_count);
     size_t start = 0;
     size_t end;
 
