6cc3179679a53846931722ed2cfc98dcfb5757ca
Wiebe Cazemier <wiebe@halfgaar.net>
2025-03-02 18:48:52 +0100

    Mark bridge session as bridge client type
    At this point, this shouldn't do anything, but fixes future cases of
    looking at the session's client type.

--- subscriptionstore.cpp
+++ subscriptionstore.cpp
@@ -329,6 +329,7 @@ std::shared_ptr<Session> SubscriptionStore::getBridgeSession(std::shared_ptr<Cli
     std::shared_ptr<Session> session = std::make_shared<Session>(client_id, client->getUsername());
     session->assignActiveConnection(client);
     client->assignSession(session);
+    session->setClientType(ClientType::LocalBridge);
     sessionsById[client_id] = session;
     return session;
 }
