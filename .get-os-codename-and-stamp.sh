27b112f2b36c93b271ad3ccf64dc1b4a71e0b41d
Wiebe Cazemier <wiebe@halfgaar.net>
2026-01-04 14:22:08 +0100

    Use deterministic debian revision in version number
    This allows for building all releases in parallel, with correct version
    numbers for dist-upgrades.

--- .get-os-codename-and-stamp.sh
+++ .get-os-codename-and-stamp.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
-stamp=$(date +%s)
-MY_CODENAME=""
+my_codename=""
+my_int_version=""
 
 if [[ -e "/etc/os-release" ]]; then
   eval "$(cat "/etc/os-release")"
-  MY_CODENAME="$VERSION_CODENAME"
+  my_codename="$VERSION_CODENAME"
+  my_int_version=${VERSION_ID//./}
 elif [[ -e "/etc/lsb-release" ]]; then
   eval "$(cat "/etc/lsb-release")"
-  MY_CODENAME="$DISTRIB_CODENAME"
+  my_codename="$DISTRIB_CODENAME"
+  my_int_version=${DISTRIB_RELEASE//./}
 else
-  echo "Error in determing os codename"
+  1>&2 echo "Error in determing os codename"
   exit 1
 fi
 
-if [[ -z "$MY_CODENAME" ]]; then
-  echo "ERROR in determining OS codename"
+if [[ -z "$my_codename" ]]; then
+  1>&2 echo "ERROR in determining OS codename"
   exit 1
 fi
 
-echo -n "${stamp}+${MY_CODENAME}"
+if [[ ! "$my_int_version" =~ ^[0-9]+$ ]]; then
+  1>&2 echo "ERROR: int version '$my_int_version' is not an int. We need a numeric string for proper debian-revision version comparison."
+  exit 1
+fi
+
+# Sequence numbers makes sure that when one upgrades the OS, the package for
+# the new distro version is selected.
+sequence="$my_int_version"
+
+if [[ -z "$sequence" ]]; then
+  1>&2 echo "ERROR: no OS sequence defined for $my_codename"
+  exit 2
+fi
+
+echo -n "${sequence}+${my_codename}+${EPOCHSECONDS}"
