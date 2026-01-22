99d025b76a70df75c0ed652f119a3d5f28ddde5e
Wiebe Cazemier <wiebe@halfgaar.net>
2026-01-22 20:20:36 +0100

    Change order of initialization and persistence loading
    This is required for the coming feature of lazy subscriptions. The
    expanding of the loaded subscriptions needs to be matched against
    bridges.

--- mainapp.h
+++ mainapp.h
@@ -78,6 +78,7 @@ class MainApp
 
     BackgroundWorker bgWorker;
 
+    void loadPersistance();
     void setSelf(const std::shared_ptr<MainApp> &self);
     bool getFuzzMode() const;
     void setlimits();
