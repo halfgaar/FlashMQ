02819ff45466e8d492f0ade2888cffc42d2df4cf
Wiebe Cazemier <wiebe@halfgaar.net>
2026-02-16 21:48:45 +0100

    Use --abort-on-first-fail in CI

--- FlashMQTests/run-tests-from-ci.sh
+++ FlashMQTests/run-tests-from-ci.sh
@@ -4,7 +4,8 @@ STDERR_LOG=$(mktemp)
 
 cd buildtests || exit 1
 
-if ./flashmq-tests 2> "$STDERR_LOG" ; then
+# Using --abort-on-first-fail because the output can be hard to find in CI when it's swamped out by the rest.
+if ./flashmq-tests --abort-on-first-fail 2> "$STDERR_LOG" ; then
   echo -e '\033[01;32mSUCCESS!\033[00m'
 else
   echo -e '\033[01;31mBummer\033[00m'
